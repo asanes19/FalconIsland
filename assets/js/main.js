3dba04a0293b4ae862d2a29fa0ecebacd5b7012e
unknown <abdelrahmansherif1930@gmail.com>
2023-09-10 20:29:16 +0400

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,42 @@
-
 /*==================== SHOW MENU ====================*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
+function linkAction() {
+    const navMenu = document.getElementById("nav-menu");
     // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+    navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
-    const header = document.getElementById('header')
+function scrollHeader() {
+    const header = document.getElementById("header");
     // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if (this.scrollY >= 100) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SWIPER DISCOVER ====================*/
 let swiper = new Swiper(".discover__container", {
@@ -50,133 +49,149 @@ let swiper = new Swiper(".discover__container", {
     coverflowEffect: {
         rotate: 0,
     },
-})
+});
 
 /*==================== VIDEO ====================*/
-const videoFile = document.getElementById('video-file'),
-      videoButton = document.getElementById('video-button'),
-      videoIcon = document.getElementById('video-icon')
+const videoFile = document.getElementById("video-file"),
+    videoButton = document.getElementById("video-button"),
+    videoIcon = document.getElementById("video-icon");
 
-function playPause(){ 
-    if (videoFile.paused){
+function playPause() {
+    if (videoFile.paused) {
         // Play video
-        videoFile.play()
+        videoFile.play();
         // We change the icon
-        videoIcon.classList.add('ri-pause-line')
-        videoIcon.classList.remove('ri-play-line')
-    }
-    else {
+        videoIcon.classList.add("ri-pause-line");
+        videoIcon.classList.remove("ri-play-line");
+    } else {
         // Pause video
-        videoFile.pause(); 
+        videoFile.pause();
         // We change the icon
-        videoIcon.classList.remove('ri-pause-line')
-        videoIcon.classList.add('ri-play-line')
-
+        videoIcon.classList.remove("ri-pause-line");
+        videoIcon.classList.add("ri-play-line");
     }
 }
-videoButton.addEventListener('click', playPause)
+videoButton.addEventListener("click", playPause);
 
-function finalVideo(){
+function finalVideo() {
     // Video ends, icon change
-    videoIcon.classList.remove('ri-pause-line')
-    videoIcon.classList.add('ri-play-line')
+    videoIcon.classList.remove("ri-pause-line");
+    videoIcon.classList.add("ri-play-line");
 }
 // ended, when the video ends
-videoFile.addEventListener('ended', finalVideo)
+videoFile.addEventListener("ended", finalVideo);
 
+/*==================== SHOW SCROLL UP ====================*/
 
-/*==================== SHOW SCROLL UP ====================*/ 
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
     // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp);
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+    const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
+        } else {
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
         }
-    })
+    });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
-    distance: '60px',
+    distance: "60px",
     duration: 2800,
     // reset: true,
-})
+});
 
-
-sr.reveal(`.home__data, .home__social-link, .home__info,
+sr.reveal(
+    `.home__data, .home__social-link, .home__info,
            .discover__container,
            .experience__data, .experience__overlay,
            .place__card,
            .sponsor__content,
-           .footer__data, .footer__rights`,{
-    origin: 'top',
-    interval: 100,
-})
+           .footer__data, .footer__rights`,
+    {
+        origin: "top",
+        interval: 100,
+    }
+);
 
-sr.reveal(`.about__data, 
+sr.reveal(
+    `.about__data, 
            .video__description,
-           .subscribe__description`,{
-    origin: 'left',
-})
+           .subscribe__description`,
+    {
+        origin: "left",
+    }
+);
 
-sr.reveal(`.about__img-overlay, 
+sr.reveal(
+    `.about__img-overlay, 
            .video__content,
-           .subscribe__form`,{
-    origin: 'right',
-    interval: 100,
-})
+           .subscribe__form`,
+    {
+        origin: "right",
+        interval: 100,
+    }
+);
 
-function view_pic(img_link){
-    const img = document.getElementById("imagev")
-    img.src = img_link
-  }
-  const TH = "assets/img/picturess/Touwnhouse.jpg"
-  
-  const twobdr = "assets/img/picturess/2BR.jpg"
+function view_pic(img_link) {
+    const img = document.getElementById("imagev");
+    img.src = img_link;
+}
+const TH = "assets/img/picturess/Touwnhouse.jpg";
 
-  const threebdr = "assets/img/picturess/3BR.jpg"
+const twobdr = "assets/img/picturess/2BR.jpg";
 
-  const fourbdr = "assets/img/picturess/4BR-semi.jpg" 
+const threebdr = "assets/img/picturess/3BR.jpg";
 
-  const fourv = "assets/img/picturess/4BR-villa.jpg"
+const fourbdr = "assets/img/picturess/4BR-semi.jpg";
 
-  const fivev = "assets/img/picturess/5BR.jpg"
+const fourv = "assets/img/picturess/4BR-villa.jpg";
 
+const fivev = "assets/img/picturess/5BR.jpg";
 
-  
-  const Townhouse = document.getElementById("Townhouse")
-  Townhouse.addEventListener("click", function(){view_pic(TH)})
-  
-  const twoBR = document.getElementById("twoBR")
-  twoBR.addEventListener("click", function(){view_pic(twobdr)})
+const Townhouse = document.getElementById("Townhouse");
+Townhouse.addEventListener("click", function () {
+    view_pic(TH);
+});
 
-  const trBR = document.getElementById("trBR")
-  trBR.addEventListener("click", function(){view_pic(threebdr)})
+const twoBR = document.getElementById("twoBR");
+twoBR.addEventListener("click", function () {
+    view_pic(twobdr);
+});
 
-  const fBR = document.getElementById("fBR")
-  fBR.addEventListener("click", function(){view_pic(fourbdr)})
+const trBR = document.getElementById("trBR");
+trBR.addEventListener("click", function () {
+    view_pic(threebdr);
+});
 
-  const fBRV = document.getElementById("fBRV")
-  fBRV.addEventListener("click", function(){view_pic(fourv)})
-  
-  const fiveBRV = document.getElementById("fiveBRV")
-  fiveBRV.addEventListener("click", function(){view_pic(fivev)})
+const fBR = document.getElementById("fBR");
+fBR.addEventListener("click", function () {
+    view_pic(fourbdr);
+});
 
+const fBRV = document.getElementById("fBRV");
+fBRV.addEventListener("click", function () {
+    view_pic(fourv);
+});
+
+const fiveBRV = document.getElementById("fiveBRV");
+fiveBRV.addEventListener("click", function () {
+    view_pic(fivev);
+});
